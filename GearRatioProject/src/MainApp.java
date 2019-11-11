0b1189e4b57a4d83c9ced02b79cf90fbb85e33e9
dkhakha9 <khak4741@stthomas.edu>
2019-11-10 22:00:00 -0600

    Initial revision.

---
+++ GearRatioProject/src/MainApp.java
@@ -0,0 +1,11 @@
+import edu.princeton.cs.introcs.StdOut;
+
+public class MainApp
+{
+
+	public static void main(String[] args)
+	{
+		StdOut.println("Thank you for using Gear Ratio App\n");
+	}
+
+}
---
+++ GearRatioProject/test/MainAppTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainAppTest
+{
+	@Test
+	public void testDummy()
+	{
+		assertFalse(true);
+	}
+	
+	
+} /* MainAppTest */
